849387d1bc8e30aaf145d4d64b69120be3864c81
JohanJvR <u22590732@tuks.co.za>
2024-08-17 20:47:28 +0200

    Update get_user_choice.asm

--- get_user_choice.asm
+++ get_user_choice.asm
@@ -8,6 +8,9 @@ section .bss
     ; ==========================
     ; Your data goes here
     ; ==========================
+    choice db 'Choice: ', 0
+    choice_len equ $-choice
+    userChoice db 0
 
 section .text
     global get_user_choice
@@ -21,5 +24,31 @@ get_user_choice:
     ; ==========================
     ; Your data goes here
     ; ==========================
+
+    ; Print Choice:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, choice
+    mov rdx, choice_len
+    syscall
+
+    ; Read user input
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, userChoice
+    mov rdx, 1
+    syscall
+
+    ; Output the user choice
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, userChoice
+    mov rdx, 1
+    syscall
+
+    mov rax, 60       
+    xor rdi, rdi    
+    syscall 
+
     ; Do not modify anything below this line unless you know what you are doing 
     ret
