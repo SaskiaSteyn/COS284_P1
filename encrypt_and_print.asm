674d48665de39b38a58399ca4bf2a8221f8d3ed7
Saskia Steyn <u17267162@tuks.co.za>
2024-08-18 18:59:24 +0200

    resotred whoopsie

--- encrypt_and_print.asm
+++ encrypt_and_print.asm
@@ -10,14 +10,20 @@ section .data
     ; ==========================
     ; Your data goes here
     ; ==========================
+    xor_key dd 0x73113777
 
 section .bss
     ; ==========================
     ; Your data goes here
     ; ==========================
+    message resb 4
 
 section .text
     global encrypt_and_print
+    textInput db "Enter plaintext to encrypt: "
+    encryptedText db "The cipher text is: "
+    messageLength equ 28
+    encryptedLength equ 20
 
 extern printf
 
@@ -32,7 +38,52 @@ print_char_32:
 encrypt_and_print:
     ; Do not modify anything above this line unless you know what you are doing
     ; ==========================
-    ; Your code goes here
+    ; Display the the message to the user
+    mov rax, 1                      ; writing the number
+    mov rdi, 1                      
+    mov rsi, textInput              ; poiting to the prompt message
+    mov rdx, messageLength          ; checking the length of the message
+    syscall                         ; perform the write
+    
+    ; Get the user's input
+    mov rax, 0                      
+    mov rdi, 0                      
+    mov rsi, message                ; buffer to store the input
+    mov rdx, 4                      ; maximum number of bytes to read (up to 4)
+    syscall                         
+
+    ; Display the output label to the user
+    mov rax, 1                      ; writing the number
+    mov rdi, 1                      
+    mov rsi, encryptedText          
+    mov rdx, encryptedLength               
+    syscall                         ; perform the write
+
+    ; Encrypt and print the message
+
+    ; Encrypting the first character
+    movzx rax, byte [message]       ; move the first char to rax
+    rol rax, 4                      ; Rotating the bit left by 4
+    xor rax, [xor_key]              ; XOR with the encryption key
+    call print_char_32              ; Printing the encrypted char
+
+    ; Encrypting the second character
+    movzx rax, byte [message+1]     ; move the second char to rax
+    rol rax, 4                      ; Rotating the bit left by 4
+    xor rax, [xor_key]              ; XOR with the encryption key
+    call print_char_32              ; Printing the encrypted char
+
+    ; Encrypting the third character
+    movzx rax, byte [message+2]     ; move the third char to rax
+    rol rax, 4                      ; Rotating the bit left by 4
+    xor rax, [xor_key]              ; XOR with the encryption key
+    call print_char_32              ; Printing the encrypted char
+
+    ; Encrypting the fourth character
+    movzx rax, byte [message+3]     ; move the fourth char to rax
+    rol rax, 4                      ; Rotating the bit left by 4
+    xor rax, [xor_key]              ; XOR with the encryption key
+    call print_char_32              ; Printing the encrypted char
     ; ==========================
     ; Do not modify anything below this line unless you know what you are doing
 
